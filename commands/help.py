18c92fa65fae07beb71843323177480e20beb805
its's me <summertriangle.dev@gmail.com>
2017-02-01 22:26:02 -0800

    fix search in =help

--- commands/help.py
+++ commands/help.py
@@ -9,11 +9,19 @@ async def help(context, message, content):
     for word in content.split():
         if word in inspect_cmd.sub_dispatch_table:
             inspect_cmd = inspect_cmd.sub_dispatch_table[word]
+            break
 
         for cmd_struct in inspect_cmd.sub_dispatch_table.values():
             if word in cmd_struct.extwords:
                 inspect_cmd = cmd_struct
                 break
+        else:
+            continue
+
+        break
+    else:
+        if content:
+            return await context.reply("No such command: `{0}`.".format(content))
 
     context.arg0 = content
     await context.reply(inspect_cmd.help_message(context))
